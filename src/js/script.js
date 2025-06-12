fac0816a38041ab55a1f0087d9cddf8ac3ed70f5
João Paulo Araújo <jpf.araujo99@hotmail.com>
2025-06-12 10:47:03 -0300

    Feat: Gerando senhas aleatórias no DOM

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,7 @@
 // Seleção de Elementos
 const generateButton = document.querySelector("#generate-password");
 const generatedElement = document.querySelector("#generated-password");
+
 // Funções
 const getLetterLowerCase = () => {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
@@ -19,7 +20,43 @@ getSymbols = () => {
   return symbols[Math.floor(Math.random() * symbols.length)];
 };
 
+const generatePassword = (
+  getLetterLowerCase,
+  getLetterUpperCase,
+  getNumber,
+  getSymbols
+) => {
+  let password = "";
+
+  const passwordLength = 10;
+
+  const generators = [
+    getLetterLowerCase,
+    getLetterUpperCase,
+    getNumber,
+    getSymbols,
+  ];
+
+  for (i = 0; i < passwordLength; i = i + generators.length) {
+    generators.forEach(() => {
+      const randomValue =
+        generators[Math.floor(Math.random() * generators.length)]();
+
+      password += randomValue;
+    });
+  }
+  password = password.slice(0, passwordLength);
+
+  generatedElement.style.display = "block";
+  generatedElement.querySelector("h4").innerText = password;
+};
+
 // Eventos
 generateButton.addEventListener("click", () => {
-  console.log("teste");
+  generatePassword(
+    getLetterLowerCase,
+    getLetterUpperCase,
+    getNumber,
+    getSymbols
+  );
 });
